3300d376457f3c9907cc9dec0ec9026245b34293
Dinesh Kumar <dineshkmr2801@gmail.com>
2019-09-13 16:44:33 +0530

    test commit 3

--- springmvc-userlogin/src/test/java/jbr/springmvc/UserServiceTest.java
+++ springmvc-userlogin/src/test/java/jbr/springmvc/UserServiceTest.java
@@ -1,18 +1,5 @@
 package jbr.springmvc;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import jbr.springmvc.model.Login;
-import jbr.springmvc.model.User;
-import jbr.springmvc.service.UserService;
-
 //@ContextConfiguration(locations = { "classpath:jbr/config/user-beans.xml" })
 //@RunWith(SpringJUnit4ClassRunner.class)
 public class UserServiceTest {
